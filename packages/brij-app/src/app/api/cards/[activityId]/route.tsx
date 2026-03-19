5b99cbe4d806e3012dfeb018e0858de942e99f43
autocatalytic <ken@extol.work>
2026-03-19 11:54:46 -0700

    Debug: remove chain icon to test card 500 error

--- packages/brij-app/src/app/api/cards/[activityId]/route.tsx
+++ packages/brij-app/src/app/api/cards/[activityId]/route.tsx
@@ -459,12 +459,7 @@ export async function GET(
                   opacity: 0.45,
                 }}
               >
-                <img
-                  src={`data:image/svg+xml,${encodeURIComponent('<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="#F5E6D0" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"/><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"/></svg>')}`}
-                  width={40}
-                  height={40}
-                />
-                Verified on Solana
+                  Verified on Solana
               </div>
               <div
                 style={{
